e4dd22022b04e3fa5676a370e33144e5d3bfd1db
Zhongshan Jiang <jzshhh@my.swjtu.edu.cn>
2024-05-16 15:33:39 +0800

    Add files via upload

--- gnss2tws_slepian/code/Decomposition/gnss_pca.m
+++ gnss2tws_slepian/code/Decomposition/gnss_pca.m
@@ -1,26 +1,38 @@
-function [coeff,score,all_explained,each_explained,data_recon]=gnss_pca(data_raw,num_pc)
+function [coeff,score,var_explained_PCA,explained,data_recon]=gnss_pca(data_raw,num_pc)
 % 
 % Description: Performing PCA decomposition using the 'als' algorithm for missing data
 % 
 % Input:
 %   data_raw         Raw observation data
-%   num_pc           Number of selected PCs, note that it may cause overfitting due to too much PCs
+%   num_pc           Number of selected PCs 
 % Output:
 %   coeff            Spatial functions
 %   score            Temporal fucntions
-%   all_explained    Variance contribution of selected PCs to raw GNSS data
-%   each_explained   Variance contribution of each PC to the filtered data
+%   all_percents     Variance contribution of selected PCs to raw GNSS data
+%   explained        Variance contribution of each PC to the filtered data
 %   data_recon       Reconstructed GNSS data
 % 
 % Author: Zhongshan Jiang
-% Date: 06/01/2023 
-% Institution: Sun Yat-Sen University 
-% E-mail: jiangzhsh@mail.sysu.edu.cn
+% Date: 11/03/2022 
+% Institution: Southwest Jiaotong University 
+% E-mail: jzshhh@my.swjtu.edu.cn
 
 %% Implementing PCA decomposition, algorithm 'als'
 h=msgbox('Implement PCA decomposition, algorithm: ''als'', please wait!');
-opt = statset('pca'); opt.MaxIter = 1000; opt.Display = 'iter';
-[coeff,score,latent,tsquared,each_explained,mu] = pca(data_raw,'algorithm','als','Centered','off','NumComponents',num_pc,'Options',opt);
+
+%% Using built-in pca.m fucntion in Matlab's Statistics and Machine Learning Toolbox
+% opt = statset('pca'); opt.MaxIter = 2000; opt.Display = 'iter'; 
+% [coeff,score,latent,tsquared,explained,mu] = pca(data_raw,'algorithm','als','Centered','off','NumComponents',num_pc,'Options',opt);
+
+%% PCA decomposition using 'als' algorithm, which is modified according to the Matlab's built-in pca.m script
+Is_Centered=false; % Centralized observation matrix (true or false)
+% Options for als algorithm, including four parameters:
+%   'Display' - Level of display output.  Choices are 'off' (the default), 'final', and 'iter'.
+%   'MaxIter' - Maximum number of steps allowed. The default is 1000. Unlike in optimization settings, reaching MaxIter is regarded as convergence.
+%   'TolFun' - Positive number giving the termination tolerance for the cost function.  The default is 1e-6.
+%    'TolX' - Positive number giving the convergence threshold for relative change in the elements of L and R.The default is 1e-6.
+opt.MaxIter = 2000; opt.Display = 'iter';opt.TolFun = 1e-6; opt.TolX = 1e-6; 
+[coeff,score,latent,explained,mu] = pca_als(data_raw,num_pc,Is_Centered,opt); 
 data_recon = score*coeff' + repmat(mu,size(data_raw,1),1);
 
 %% Calcuating variance contribution of selected PCs to raw GNSS data
@@ -35,7 +47,7 @@ for i=1:1:no_site
     var_r(i,1)=r'*r;
     Var_d(i,1)=d'*d;
 end
-all_explained=100*(1-sum(var_r)/sum(Var_d));
-fprintf('variance explained PCA = %f%%\n', all_explained);
+var_explained_PCA=100*(1-sum(var_r)/sum(Var_d));
+fprintf('variance explained PCA = %f%%\n', var_explained_PCA);
 close(h);
 
